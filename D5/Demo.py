fb188d47e7db015c03f2984a77918baeac937782
Mahek2000 <85340598+Mahek2000@users.noreply.github.com>
2021-06-15 20:30:20 +0530

    Add files via upload

---
+++ D5/Demo.py
@@ -0,0 +1,17 @@
+class Demo:
+    def myfunction(self):
+        print("This is my fucation of class...")
+
+
+    def show(self,name):
+        print("value is ",name)
+
+    def fun1(self,n1,n2):
+            ans = n1 + n2
+            print("Ans is ",ans)
+
+
+d1=Demo()
+d1.myfunction()
+d1.show("Mahek kalariya") 
+d1.fun1(10,20) 
---
+++ D5/Function Demo.py
@@ -0,0 +1,16 @@
+class Demo:
+    def myfunction(self):
+        print("This is my function of class...")
+
+
+    def show(self,name):
+        print("value is ",name)
+
+    def __init__(self,nm):
+        print("This is demo class... ")
+        print("Name is :",nm)
+
+
+d1=Demo('class')
+d1.myfunction()
+d1.show("Mahek kalariya") 
